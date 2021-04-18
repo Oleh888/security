fcb5859b627d8e18e7f50b1bb5b590d0b3e9ea34
Oleh <oleg24453@gmail.com>
2021-04-18 12:09:51 +0300

    added ON CONFLICT option to data.sql script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,17 @@
 INSERT INTO users (id, username, password, algoritm)
-    VALUES ('1', 'oleh', '$2a$10$xn3LI/AjqicFYZFruSwve.681477XaVNaUQbr1gioaWPn4t1KsnmG', 'BCRYPT');
+    VALUES ('1', 'oleh', '$2a$10$xn3LI/AjqicFYZFruSwve.681477XaVNaUQbr1gioaWPn4t1KsnmG', 'BCRYPT')
+    ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO authority (
     id, name, user_id)
-    VALUES ('1', 'READ', '1');
+    VALUES ('1', 'READ', '1')
+    ON CONFLICT (id) DO NOTHING;
 INSERT INTO authority (
     id, name, user_id)
-    VALUES ('2', 'WRITE', '1');
+    VALUES ('2', 'WRITE', '1')
+    ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO product (
     id, name, price, currency)
-    VALUES ('1', 'Car', '100000', 'USD');
+    VALUES ('1', 'Car', '100000', 'USD')
+    ON CONFLICT (id) DO NOTHING;
